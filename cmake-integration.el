59376bf08a3c6cca942745dd5a463776716e0e6f
Darlan Caalcante Moreira <darcamo@gmail.com>
2025-02-05 18:44:24 -0300

    Add command to run ctest

--- cmake-integration.el
+++ cmake-integration.el
@@ -1096,6 +1096,31 @@ from where the doxygen command will be run."
   )
 
 ;; xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+;; xxxxxxxxxxxxxxx CTest xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+;; xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+(defun cmake-integration--get-ctest-command (&optional only-failed?)
+  "Get the command to run all tests, or only failed ones if ONLY-FAILED?.
+
+If only-failed? is t, then \"--rerun-failed --output-on-failure\" is
+added to the ctest command."
+  (let* ((preset-name (cmake-integration-get-last-configure-preset-name))
+         (build-folder (cmake-integration-get-build-folder))
+         (ctest-command (if preset-name
+                            (format "ctest --preset %s" preset-name)
+                          (format "cd %s && ctest ." build-folder))))
+    (if only-failed?
+        (format "%s %s" ctest-command "--rerun-failed --output-on-failure")
+      ctest-command)))
+
+
+;;;###autoload
+(defun cmake-integration-run-ctest ()
+  "Run ctest."
+  (interactive)
+  (compile (cmake-integration--get-ctest-command current-prefix-arg)))
+
+
+;; xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 (provide 'cmake-integration)
 
 ;;; cmake-integration.el ends here
