a1d9f72d1020f6e1d25f9b5523d3b613ad5544dc
rumboon <35624272+rumboon@users.noreply.github.com>
2025-03-01 23:22:37 +0100

    Fix use-package from failing to process the package-requires list

--- cmake-integration.el
+++ cmake-integration.el
@@ -2,7 +2,7 @@
 
 ;; Author: Darlan Cavalcante Moreira <darcamo@gmail.com>
 ;; Version: 0.1
-;; Package-Requires: ((emacs "28.1") f s json)
+;; Package-Requires: ((emacs "28.1") (f "0.20.0") (s "1.12.0") (json "1.5") (dash "2.19.1"))
 ;; Homepage: https://github.com/darcamo/cmake-integration
 ;; Keywords: c c++ cmake languages tools
 ;; URL: https://github.com/darcamo/cmake-integration/
@@ -35,6 +35,7 @@
 (require 'f)
 (require 's)
 (require 'json)
+(require 'dash)
 (require 'cl-extra)
 
 (require 'cmake-integration-variables)
