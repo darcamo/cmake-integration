9dda0c5d41e239bbf4a73c2b10fc62c410327edd
Darlan Cavalcante Moreira <darcamo@gmail.com>
2025-10-09 14:04:14 -0300

    Add some placeholder files for possible new functionality

---
+++ cmake-integration-binary-info.el
@@ -0,0 +1,18 @@
+
+
+(require 'cmake-integration-launch)
+
+;; TODO Get information from a target binary
+;; - RPATH or RUNPATH
+;; - SONAME for libraries
+;; - Dependencies (other libraries)
+;; - Symbols (exported, imported, undefined)
+;; - Build ID
+;; - Debug info (if any)
+;; - Whether it is stripped or not
+;; - Whether it is a PIE or not
+;; - Whether it is statically or dynamically linked
+;; - Whether it is a shared library or not
+;; - Whether it is an executable or not
+;; - Whether it is a position-independent executable or not
+;; - Whether it is a relocatable or not
---
+++ cmake-integration-toolchains.el
@@ -0,0 +1,18 @@
+;;; cmake-integration-toolchains.el --- Get information about the toolchains -*- lexical-binding: t -*-
+
+;;; Commentary:
+
+;;; Code:
+
+
+;; TODO Use the CMake file API to get information about the toolchains
+;; See https://cmake.org/cmake/help/latest/manual/cmake-file-api.7.html#object-kind-toolchains
+
+
+(provide 'cmake-integration-toolchains)
+
+;;; cmake-integration-toolchains.el ends here
+
+;; Local Variables:
+;; read-symbol-shorthands: (("ci-" . "cmake-integration-"))
+;; End:
