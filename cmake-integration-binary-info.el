34ae086e31c4dfc8b562f9bd4d8f98fdc0a4ffe3
Darlan Cavalcante Moreira <darcamo@gmail.com>
2025-10-09 17:47:50 -0300

    Add function to get RUNPATH and SONAME of  a binary

--- cmake-integration-binary-info.el
+++ cmake-integration-binary-info.el
@@ -1,4 +1,4 @@
-;;; cmake-integration-binary-info.el --- Get information about binaries -*- lexical-binding: t -*-
+;;; cmake-integration-binary-info.el --- Get information about target binaries -*- lexical-binding: t -*-
 
 ;; Copyright (C) 2025 Darlan Cavalcante Moreira
 
@@ -43,3 +43,52 @@
 ;; - Whether it is an executable or not
 ;; - Whether it is a position-independent executable or not
 ;; - Whether it is a relocatable or not
+
+
+(defun ci-get-binary-runpath (binary-path)
+  "Get the RUNPATH of the binary in BINARY-PATH."
+  (let ((binary-path (expand-file-name binary-path)))
+    (if (not (file-exists-p binary-path))
+        (message "Binary file does not exist: %s" binary-path)
+      (with-temp-buffer
+        (let ((exit-code (call-process "readelf" nil t nil "-d" binary-path)))
+          (if (/= exit-code 0)
+              (message "Failed to run readelf on %s" binary-path)
+            (goto-char (point-min))
+            (let ((result))
+              (if (re-search-forward "RUNPATH.*\\[\\(.*?\\)\\]" nil t)
+                  (setq result (match-string 1))
+                (if (re-search-forward "RPATH.*\\[\\(.*?\\)\\]" nil t)
+                    (setq result (match-string 1))
+                  nil))
+              (when result
+                (split-string result ":")))))))))
+
+
+(defun ci-get-current-target-runpath ()
+  "Get the RUNPATH of the binary for the current target."
+  (interactive)
+  (ci-get-binary-runpath (ci-get-target-executable-full-path)))
+
+
+(defun ci-get-library-soname (library-path)
+  "Get the SONAME of the shared library in LIBRARY-PATH."
+  (interactive)
+  (let ((library-path (expand-file-name library-path)))
+    (if (not (file-exists-p library-path))
+        (message "Library file does not exist: %s" library-path)
+      (with-temp-buffer
+        (let ((exit-code (call-process "readelf" nil t nil "-d" library-path)))
+          (if (/= exit-code 0)
+              (message "Failed to run readelf on %s" library-path)
+            (goto-char (point-min))
+            (if (re-search-forward "SONAME.*\\[\\(.*?\\)\\]" nil t)
+                (match-string 1))))))))
+
+
+
+;;; cmake-integration-binary-info.el ends here
+
+;; Local Variables:
+;; read-symbol-shorthands: (("ci-" . "cmake-integration-"))
+;; End:
