bdb6601df2f12c56058302b6b45601a8f195937c
Darlan Cavalcante Moreira <darcamo@gmail.com>
2025-10-09 14:04:14 -0300

    Add some placeholder files for possible new functionality

---
+++ cmake-integration-folder-view.el
@@ -0,0 +1,44 @@
+;;; cmake-integration-folder-view.el --- Get a folder view of the CMake targets -*- lexical-binding: t -*-
+
+;; Copyright (C) 2025 Darlan Cavalcante Moreira
+
+;; Author: Darlan Cavalcante Moreira <darcamo@gmail.com>
+
+;; SPDX-License-Identifier: GPL-3.0-or-later
+
+;; This file is not part of GNU Emacs
+
+;; This file is part of cmake-integration.
+
+;; cmake-integration is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; cmake-integration is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with cmake-integration. If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;; Code:
+
+
+;; TODO Generate a folder view for CMake targets.
+;; See https://cmake.org/cmake/help/latest/prop_tgt/FOLDER.html
+
+
+
+
+(provide 'cmake-integration-folder-view)
+
+;;; cmake-integration-folder-view.el ends here
+
+;; Local Variables:
+;; read-symbol-shorthands: (("ci-" . "cmake-integration-"))
+;; End:
+
