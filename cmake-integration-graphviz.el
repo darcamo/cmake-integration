dc784c0ec19ac24b87f8a998b5e685f386f52ce4
Darlan Cavalcante Moreira <darcamo@gmail.com>
2025-10-09 14:04:14 -0300

    Add some placeholder files for possible new functionality

---
+++ cmake-integration-graphviz.el
@@ -0,0 +1,43 @@
+;;; cmake-integration-graphviz.el --- Visualize the targets relationship with `cmake --graphviz` -*- lexical-binding: t -*-
+
+;; Copyright (C) 2025 Darlan Cavalcante Moreira
+
+;; Author: Darlan Cavalcante Moreira <darcamo@gmail.com>
+
+;; SPDX-License-Identifier: GPL-3.0-or-later
+
+;; This file is not part of GNU Emacs
+
+;; This file is part of cmake-integration.
+
+;; cmake-integration is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; cmake-integration is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with cmake-integration. If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;; Code:
+
+
+;; TODO Use the "--graphviz" option of CMake to generate a Graphviz file, render it,
+;; and display it in Emacs or externaly.
+
+
+
+(provide 'cmake-integration-graphviz)
+
+;;; cmake-integration-graphviz.el ends here
+
+;; Local Variables:
+;; read-symbol-shorthands: (("ci-" . "cmake-integration-"))
+;; End:
+
