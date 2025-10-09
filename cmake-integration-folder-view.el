a8f1f950151085c8fbd582c0a11d6244735f8312
Darlan Cavalcante Moreira <darcamo@gmail.com>
2025-10-09 14:04:14 -0300

    Add some placeholder files for possible new functionality

---
+++ cmake-integration-folder-view.el
@@ -0,0 +1,21 @@
+;;; cmake-integration-folder-view.el --- Get a folder view of the CMake targets -*- lexical-binding: t -*-
+
+;;; Commentary:
+
+;;; Code:
+
+
+;; TODO Generate a folder view for CMake targets.
+;; See https://cmake.org/cmake/help/latest/prop_tgt/FOLDER.html
+
+
+
+
+(provide 'cmake-integration-folder-view)
+
+;;; cmake-integration-folder-view.el ends here
+
+;; Local Variables:
+;; read-symbol-shorthands: (("ci-" . "cmake-integration-"))
+;; End:
+
---
+++ cmake-integration-graphviz.el
@@ -0,0 +1,20 @@
+;;; cmake-integration-graphviz.el --- Visualize the targets relationship with `cmake --graphviz` -*- lexical-binding: t -*-
+
+;;; Commentary:
+
+;;; Code:
+
+
+;; TODO Use the "--graphviz" option of CMake to generate a Graphviz file, render it,
+;; and display it in Emacs or externaly.
+
+
+
+(provide 'cmake-integration-graphviz)
+
+;;; cmake-integration-graphviz.el ends here
+
+;; Local Variables:
+;; read-symbol-shorthands: (("ci-" . "cmake-integration-"))
+;; End:
+
