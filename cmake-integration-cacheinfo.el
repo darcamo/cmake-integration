ff2e2176580daedd0f3bf4469f7d7c04d84bcc73
Darlan Cavalcante Moreira <darcamo@gmail.com>
2025-10-09 14:04:14 -0300

    Add some placeholder files for possible new functionality

---
+++ cmake-integration-cacheinfo.el
@@ -0,0 +1,22 @@
+;;; cmake-integration-cacheinfo.el --- Get information from the CMake cache variables -*- lexical-binding: t -*-
+
+;;; Commentary:
+
+;;; Code:
+
+
+;; TODO Use the CMake file API to get information about the cache variables
+;; See https://cmake.org/cmake/help/latest/manual/cmake-file-api.7.html#object-kind-cache
+
+
+
+
+
+(provide 'cmake-integration-cacheinfo)
+
+;;; cmake-integration-cacheinfo.el ends here
+
+;; Local Variables:
+;; read-symbol-shorthands: (("ci-" . "cmake-integration-"))
+;; End:
+
