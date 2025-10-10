f4386a8753c0637c069352d212f997d0f265132b
Darlan Cavalcante Moreira <darcamo@gmail.com>
2025-10-10 09:18:39 -0300

    Add a function to get dynamic dependencies from a binary

--- cmake-integration-binary-info.el
+++ cmake-integration-binary-info.el
@@ -65,10 +65,8 @@
                 (split-string result ":")))))))))
 
 
-(defun ci-get-current-target-runpath ()
-  "Get the RUNPATH of the binary for the current target."
-  (interactive)
-  (ci-get-binary-runpath (ci-get-target-executable-full-path)))
+;; Define ci-get-binary-rpath as an alias for ci-get-binary-runpath
+(defalias 'ci-get-binary-rpath 'ci-get-binary-runpath)
 
 
 (defun ci-get-library-soname (library-path)
@@ -86,6 +84,48 @@
                 (match-string 1))))))))
 
 
+(defun ci-get-binary-dependencies (binary-path)
+  "Get the dependencies of the binary in BINARY-PATH."
+  (let ((binary-path (expand-file-name binary-path)))
+    (if (not (file-exists-p binary-path))
+        (message "Binary file does not exist: %s" binary-path)
+      (with-temp-buffer
+        (let ((exit-code (call-process "ldd" nil t nil binary-path)))
+          (if (/= exit-code 0)
+              (message "Failed to run ldd on %s" binary-path)
+            (goto-char (point-min))
+            (let ((result '()))
+              (while (re-search-forward "=> \\(.*?\\) (" nil t)
+                (let ((lib (match-string 1)))
+                  (unless (string= lib "not found")
+                    (push lib result))))
+              (nreverse result))))))))
+
+
+(defun ci-get-current-target-runpath ()
+  "Get the RUNPATH of the binary for the current target."
+  (interactive)
+  (ci-get-binary-runpath (ci-get-target-executable-full-path)))
+
+
+;; Define an alias for ci-get-current-target-rpath
+(defalias 'ci-get-current-target-rpath 'ci-get-current-target-runpath)
+
+
+(defun ci-get-current-target-soname ()
+  "Get the SONAME of the library for the current target."
+  (interactive)
+  (ci-get-library-soname (ci-get-target-executable-full-path))
+  )
+
+
+(defun ci-get-current-target-dependencies ()
+  "Get the dependencies of the binary for the current target."
+  (interactive)
+  (ci-get-binary-dependencies (ci-get-target-executable-full-path))
+  )
+
+(provide 'cmake-integration-binary-info)
 
 ;;; cmake-integration-binary-info.el ends here
 
