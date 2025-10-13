af0c4fbfeb64ff5f9d0dda192fcde591a5d1b9a6
Darlan Cavalcante Moreira <darcamo@gmail.com>
2025-10-13 18:08:42 -0300

    WIP

--- cmake-integration-binary-info.el
+++ cmake-integration-binary-info.el
@@ -45,9 +45,27 @@
 ;; - Whether it is a relocatable or not
 
 
-(defun ci-get-binary-runpath (binary-path)
-  "Get the RUNPATH of the binary in BINARY-PATH."
-  (let ((binary-path (expand-file-name binary-path)))
+;; (defun ci--get-initial-file-name-if-in-dired ()
+;;   "Return the file at point if in Dired mode, otherwise nil."
+;;   (if (derived-mode-p 'dired-mode)
+;;       (dired-get-file-for-visit)
+;;     nil))
+
+
+(defun ci--maybe-ask-user-for-filename (&optional binary-path)
+  "If BINARY-PATH is nil, prompt the user to select a file.
+
+If BINARY-PATH is non-nil, return it as is."
+  (if binary-path
+      (expand-file-name binary-path)
+    (expand-file-name (read-file-name "Select a file: "))))
+
+
+(defun ci--get-binary-runpath (&optional binary-path)
+  "Get the RUNPATH of the binary in BINARY-PATH.
+
+If BINARY-PATH is nil, prompt the user to select a file."
+  (let ((binary-path (ci--maybe-ask-user-for-filename binary-path)))
     (if (not (file-exists-p binary-path))
         (message "Binary file does not exist: %s" binary-path)
       (with-temp-buffer
@@ -65,14 +83,22 @@
                 (split-string result ":")))))))))
 
 
-;; Define ci-get-binary-rpath as an alias for ci-get-binary-runpath
-(defalias 'ci-get-binary-rpath 'ci-get-binary-runpath)
-
-
-(defun ci-get-library-soname (library-path)
-  "Get the SONAME of the shared library in LIBRARY-PATH."
+(defun ci-print-binary-runpath (&optional binary-path)
+  "Print the RUNPATH of the binary in BINARY-PATH."
   (interactive)
-  (let ((library-path (expand-file-name library-path)))
+  (let ((runpaths (ci--get-binary-runpath binary-path)))
+    (if (not runpaths)
+        (message "No RUNPATH or RPATH found.")
+      (message (string-join runpaths "\n")))))
+
+
+;; ;; Define ci-get-binary-rpath as an alias for ci--get-binary-runpath
+;; (defalias 'ci-get-binary-rpath 'ci--get-binary-runpath)
+
+
+(defun ci-get-library-soname (&optional library-path)
+  "Get the SONAME of the shared library in LIBRARY-PATH."
+  (let ((library-path (ci--maybe-ask-user-for-filename library-path)))
     (if (not (file-exists-p library-path))
         (message "Library file does not exist: %s" library-path)
       (with-temp-buffer
@@ -84,6 +110,15 @@
                 (match-string 1))))))))
 
 
+(defun ci-print-library-soname (&optional library-path)
+  "Print the SONAME of the shared library in LIBRARY-PATH."
+  (interactive)
+  (let ((soname (ci-get-library-soname library-path)))
+    (if (not soname)
+        (message "No SONAME found.")
+      (message "SONAME is '%s'" soname))))
+
+
 (defun ci-get-binary-dependencies (binary-path)
   "Get the dependencies of the binary in BINARY-PATH."
   (let ((binary-path (expand-file-name binary-path)))
@@ -92,7 +127,9 @@
       (with-temp-buffer
         (let ((exit-code (call-process "ldd" nil t nil binary-path)))
           (if (/= exit-code 0)
-              (message "Failed to run ldd on %s" binary-path)
+              (progn
+                (message "Failed to run ldd on %s" binary-path)
+                nil)
             (goto-char (point-min))
             (let ((result '()))
               (while (re-search-forward "=> \\(.*?\\) (" nil t)
@@ -104,26 +141,48 @@
 
 (defun ci-get-current-target-runpath ()
   "Get the RUNPATH of the binary for the current target."
+  (ci--get-binary-runpath (ci-get-target-executable-full-path)))
+
+
+(defun ci-print-current-target-runpath ()
+  "Print the RUNPATH of the binary for the current target."
   (interactive)
-  (ci-get-binary-runpath (ci-get-target-executable-full-path)))
+  (let ((runpaths (ci-get-current-target-runpath)))
+    (if (not runpaths)
+        (message "No RUNPATH or RPATH found.")
+      (message (string-join runpaths "\n")))))
 
-
-;; Define an alias for ci-get-current-target-rpath
-(defalias 'ci-get-current-target-rpath 'ci-get-current-target-runpath)
+;; ;; Define an alias for ci-get-current-target-rpath
+;; (defalias 'ci-get-current-target-rpath 'ci-get-current-target-runpath)
 
 
 (defun ci-get-current-target-soname ()
   "Get the SONAME of the library for the current target."
+  (ci-get-library-soname (ci-get-target-executable-full-path)))
+
+
+(defun ci-print-current-target-soname ()
+  "Print the SONAME of the library for the current target."
   (interactive)
-  (ci-get-library-soname (ci-get-target-executable-full-path))
-  )
+  (let ((soname (ci-get-current-target-soname)))
+    (if (not soname)
+        (message "No SONAME found.")
+      (message "SONAME is '%s'" soname))))
 
 
 (defun ci-get-current-target-dependencies ()
   "Get the dependencies of the binary for the current target."
+  (ci-get-binary-dependencies (ci-get-target-executable-full-path)))
+
+
+(defun ci-print-current-target-dependencies ()
+  "Print the dependencies of the binary for the current target."
   (interactive)
-  (ci-get-binary-dependencies (ci-get-target-executable-full-path))
-  )
+  (let ((deps (ci-get-current-target-dependencies)))
+    (if (not deps)
+        (message "No dependencies found.")
+      (message (string-join deps "\n")))))
+
 
 (provide 'cmake-integration-binary-info)
 
